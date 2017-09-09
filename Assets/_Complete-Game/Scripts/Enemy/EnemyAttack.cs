f0922490b3db38da619b66fb7915c8b4876c7b21
Ido Yehieli <ido.yehieli@gmail.com>
2017-09-09 12:48:46 +0200

    enemy classes extend saveable, to be implemented

--- Assets/_Complete-Game/Scripts/Enemy/EnemyAttack.cs
+++ Assets/_Complete-Game/Scripts/Enemy/EnemyAttack.cs
@@ -1,86 +1,94 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace CompleteProject
-{
-    public class EnemyAttack : MonoBehaviour
-    {
-        public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
-        public int attackDamage = 10;               // The amount of health taken away per attack.
+namespace CompleteProject {
+	[System.Serializable]
+	public class EnemyAttackData: IData {
+		
+	}
+
+	public class EnemyAttack : Savable {
+		// The time in seconds between each attack.
+		public float timeBetweenAttacks = 0.5f;
+		// The amount of health taken away per attack.
+		public int attackDamage = 10;
 
 
-        Animator anim;                              // Reference to the animator component.
-        GameObject player;                          // Reference to the player GameObject.
-        PlayerHealth playerHealth;                  // Reference to the player's health.
-        EnemyHealth enemyHealth;                    // Reference to this enemy's health.
-        bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
-        float timer;                                // Timer for counting up to the next attack.
+		// Reference to the animator component.
+		Animator anim;
+		// Reference to the player GameObject.
+		GameObject player;
+		// Reference to the player's health.
+		PlayerHealth playerHealth;
+		// Reference to this enemy's health.
+		EnemyHealth enemyHealth;
+		// Whether player is within the trigger collider and can be attacked.
+		bool playerInRange;
+		// Timer for counting up to the next attack.
+		float timer;
 
 
-        void Awake ()
-        {
-            // Setting up the references.
-            player = GameObject.FindGameObjectWithTag ("Player");
-            playerHealth = player.GetComponent <PlayerHealth> ();
-            enemyHealth = GetComponent<EnemyHealth>();
-            anim = GetComponent <Animator> ();
-        }
+		void Awake () {
+			// Setting up the references.
+			player = GameObject.FindGameObjectWithTag ("Player");
+			playerHealth = player.GetComponent <PlayerHealth> ();
+			enemyHealth = GetComponent<EnemyHealth> ();
+			anim = GetComponent <Animator> ();
+		}
+
+		#region ISavable implementation
+
+		public override void LoadData (IData d) {
+			throw new System.NotImplementedException ();
+		}
+
+		#endregion
+
+		void OnTriggerEnter (Collider other) {
+			// If the entering collider is the player...
+			if (other.gameObject == player) {
+				// ... the player is in range.
+				playerInRange = true;
+			}
+		}
 
 
-        void OnTriggerEnter (Collider other)
-        {
-            // If the entering collider is the player...
-            if(other.gameObject == player)
-            {
-                // ... the player is in range.
-                playerInRange = true;
-            }
-        }
+		void OnTriggerExit (Collider other) {
+			// If the exiting collider is the player...
+			if (other.gameObject == player) {
+				// ... the player is no longer in range.
+				playerInRange = false;
+			}
+		}
 
 
-        void OnTriggerExit (Collider other)
-        {
-            // If the exiting collider is the player...
-            if(other.gameObject == player)
-            {
-                // ... the player is no longer in range.
-                playerInRange = false;
-            }
-        }
+		void Update () {
+			// Add the time since Update was last called to the timer.
+			timer += Time.deltaTime;
+
+			// If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
+			if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0) {
+				// ... attack.
+				Attack ();
+			}
+
+			// If the player has zero or less health...
+			if (playerHealth.currentHealth <= 0) {
+				// ... tell the animator the player is dead.
+				anim.SetTrigger ("PlayerDead");
+			}
+		}
 
 
-        void Update ()
-        {
-            // Add the time since Update was last called to the timer.
-            timer += Time.deltaTime;
+		void Attack () {
+			// Reset the timer.
+			timer = 0f;
 
-            // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
-            if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
-            {
-                // ... attack.
-                Attack ();
-            }
-
-            // If the player has zero or less health...
-            if(playerHealth.currentHealth <= 0)
-            {
-                // ... tell the animator the player is dead.
-                anim.SetTrigger ("PlayerDead");
-            }
-        }
-
-
-        void Attack ()
-        {
-            // Reset the timer.
-            timer = 0f;
-
-            // If the player has health to lose...
-            if(playerHealth.currentHealth > 0)
-            {
-                // ... damage the player.
-                playerHealth.TakeDamage (attackDamage);
-            }
-        }
-    }
+			// If the player has health to lose...
+			if (playerHealth.currentHealth > 0) {
+				// ... damage the player.
+				playerHealth.TakeDamage (attackDamage);
+			}
+		}
+	}
 }
--- Assets/_Complete-Game/Scripts/Enemy/EnemyMovement.cs
+++ Assets/_Complete-Game/Scripts/Enemy/EnemyMovement.cs
@@ -1,40 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace CompleteProject
-{
-    public class EnemyMovement : MonoBehaviour
-    {
-        Transform player;               // Reference to the player's position.
-        PlayerHealth playerHealth;      // Reference to the player's health.
-        EnemyHealth enemyHealth;        // Reference to this enemy's health.
-        UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
+namespace CompleteProject {
+	[System.Serializable]
+	public class EnemyMovementData: IData {
+
+	}
+
+	public class EnemyMovement : Savable {
+		// Reference to the player's position.
+		Transform player;
+		// Reference to the player's health.
+		PlayerHealth playerHealth;
+		// Reference to this enemy's health.
+		EnemyHealth enemyHealth;
+		// Reference to the nav mesh agent.
+		UnityEngine.AI.NavMeshAgent nav;
 
 
-        void Awake ()
-        {
-            // Set up the references.
-            player = GameObject.FindGameObjectWithTag ("Player").transform;
-            playerHealth = player.GetComponent <PlayerHealth> ();
-            enemyHealth = GetComponent <EnemyHealth> ();
-            nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
-        }
+		void Awake () {
+			// Set up the references.
+			player = GameObject.FindGameObjectWithTag ("Player").transform;
+			playerHealth = player.GetComponent <PlayerHealth> ();
+			enemyHealth = GetComponent <EnemyHealth> ();
+			nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+		}
 
+		#region implemented abstract members of Savable
 
-        void Update ()
-        {
-            // If the enemy and the player have health left...
-            if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-            {
-                // ... set the destination of the nav mesh agent to the player.
-                nav.SetDestination (player.position);
-            }
+		public override void LoadData (IData d) {
+			throw new System.NotImplementedException ();
+		}
+
+		#endregion
+
+		void Update () {
+			// If the enemy and the player have health left...
+			if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0) {
+				// ... set the destination of the nav mesh agent to the player.
+				nav.SetDestination (player.position);
+			}
             // Otherwise...
-            else
-            {
-                // ... disable the nav mesh agent.
-                nav.enabled = false;
-            }
-        }
-    }
+            else {
+				// ... disable the nav mesh agent.
+				nav.enabled = false;
+			}
+		}
+	}
 }
